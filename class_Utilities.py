21abd91ce1394929e3fe4742e0b55c9979553ce6
EruditeCode <erudite.home@gmail.com>
2023-01-17 16:14:15 +0000

    Added an EdgeLabel class to manage the weight labels.

--- class_Utilities.py
+++ class_Utilities.py
@@ -13,3 +13,29 @@ class Button:
 			self.pos[1] + self.height >= click_position[1] >= self.pos[1]):
 			return True
 		return False
+
+class EdgeLabel:
+	def __init__(self, width, height, nodes, nodes_dict):
+		self.width = width
+		self.height = height
+		self.nodes = nodes
+		self.weight = None
+		self.pos = None
+		self.editable = False
+
+		self.update(nodes_dict)
+
+	def update(self, nodes_dict):
+		# Update position.
+		posA = nodes_dict[self.nodes[0]].pos
+		posB = nodes_dict[self.nodes[1]].pos
+		self.pos = ((posA[0] + posB[0])//2, ((posA[1] + posB[1])//2)-15)
+
+		# Update weight.
+		self.weight = nodes_dict[self.nodes[0]].weights[self.nodes[1]]
+
+	def check_click(self, click_position):
+		if (self.pos[0] + self.width >= click_position[0] >= self.pos[0] and
+			self.pos[1] + self.height >= click_position[1] >= self.pos[1]):
+			return True
+		return False
