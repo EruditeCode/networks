9b59b143df6ad787ef5dcc2f23df735d4e466c69
EruditeCode <erudite.home@gmail.com>
2023-01-17 16:16:21 +0000

    Added functions to manage edge labels and enable updating of labels.

--- class_Network.py
+++ class_Network.py
@@ -3,6 +3,7 @@
 from class_Node import Node
 from random import randint
 from collections import deque
+from class_Utilities import EdgeLabel
 
 class Network:
 	def __init__(self, width, height, height_min=0):
@@ -12,6 +13,7 @@ class Network:
 		self.start, self.select = None, None
 		self.path = None
 		self.nodes = {}
+		self.edge_labels = []
 
 	def update(self, mouse_position, mouse_down, mouse_click, click_type):
 		# Update start and select based on click.
@@ -19,6 +21,11 @@ class Network:
 			self.check_if_node_selected(mouse_position)
 			if self.start == None:
 				self.start = self.select
+			if self.select == None:
+				self.start = self.select
+			# Reset all labels to editable = False
+			for label in self.edge_labels:
+				label.editable = False
 
 		# Adding Nodes.
 		if self.state == 0 and mouse_click and mouse_position[1] > self.height_min:
@@ -26,6 +33,7 @@ class Network:
 		# Adding Edges.
 		elif self.state == 1 and mouse_click and mouse_position[1] > self.height_min:
 			self.add_or_remove_edge(mouse_position, click_type)
+			self.check_if_label_selected(mouse_position)
 		# Moving Nodes Around.
 		elif self.state == 2 and mouse_down:
 			for key in self.nodes.keys():
@@ -41,12 +49,21 @@ class Network:
 			self.path = self.find_path(self.start, self.select)
 			self.start = None
 
+		for label in self.edge_labels:
+			label.update(self.nodes)
+
 		self.select = None
+
 
 	def check_if_node_selected(self, mouse_position):
 		for key in self.nodes.keys():
 			if self.nodes[key].is_selected(mouse_position):
 				self.select = key
+
+	def check_if_label_selected(self, mouse_position):
+		for label in self.edge_labels:
+			if label.check_click(mouse_position):
+				label.editable = True
 
 	def add_or_remove_node(self, mouse_position, click_type):
 		if click_type == 0 and self.nodes == {}:
@@ -57,25 +74,53 @@ class Network:
 		elif click_type == 1 and self.nodes and self.select:
 			for key in self.nodes.keys():
 				self.remove_edge_from_connections(self.select, key)
+				self.remove_edge_label(self.select, key)
 			del self.nodes[self.select]
 
 	def add_or_remove_edge(self, mouse_position, click_type):
-		if click_type == 0 and self.start != self.select:
+		if click_type == 0 and self.start != self.select and self.select:
 			self.add_edge_to_connections(self.start, self.select)
 			self.add_edge_to_connections(self.select, self.start)
+			self.add_edge_label(self.start, self.select)
 			self.start = None
-		elif click_type == 1 and self.start != self.select:
+		elif click_type == 1 and self.start != self.select and self.select:
 			self.remove_edge_from_connections(self.start, self.select)
 			self.remove_edge_from_connections(self.select, self.start)
+			self.remove_edge_label(self.start, self.select)
 			self.start = None
 
 	def remove_edge_from_connections(self, node_start, node_end):
 		if node_start in self.nodes[node_end].connections:
 			self.nodes[node_end].connections.remove(node_start)
+			del self.nodes[node_end].weights[node_start]
 
 	def add_edge_to_connections(self, node_start, node_end):
 		if node_end not in self.nodes[node_start].connections:
 			self.nodes[node_start].connections.append(node_end)
+			self.nodes[node_start].weights[node_end] = 0
+
+	def add_edge_label(self, node_start, node_end):
+		for label in self.edge_labels:
+			if node_start in label.nodes and node_end in label.nodes:
+				break
+		else:
+			self.edge_labels.append(EdgeLabel(50, 30, [node_start, node_end], self.nodes))
+
+	def remove_edge_label(self, node_start, node_end):
+		for label in self.edge_labels:
+			if node_start in label.nodes and node_end in label.nodes:
+				self.edge_labels.remove(label)
+				break
+
+	def update_edge_weight(self, weight):
+		for label in self.edge_labels:
+			if label.editable == True:
+				nodes = label.nodes
+				label.editable = False
+				break
+		self.nodes[nodes[0]].weights[nodes[1]] = int(weight)
+		self.nodes[nodes[1]].weights[nodes[0]] = int(weight)
+
 
 	def search(self, start, end):
 		queue, visited = deque([start]), []
